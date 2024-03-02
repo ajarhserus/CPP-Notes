2db1b60241bfe6636155fb24a6cfe79d2f90496e
ajarhserus <32294157+ajarhserus@users.noreply.github.com>
2024-03-02 09:58:15 +0530

    Update UniquePointer.cpp

--- UniquePointer.cpp
+++ UniquePointer.cpp
@@ -1,1 +1,6 @@
-
+//Title --> Unique Pointer
+/*Description & Usage:
+    Unique pointer is a smart pointer that provides exclusive ownership for the dynamically allocated objects.
+    It means the multiple objects can't share the same resource . It can be only moved or in other words, the ownership
+    can be transferred to  other object using std::move().
+*/
