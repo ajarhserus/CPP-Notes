4a1425aea6408f8b73b65606cc7eb6a5b89088a9
ajarhserus <sureshraja21@outlook.com>
2024-03-02 10:29:41 +0530

    shared pointers

---
+++ SharedPointer.cpp
@@ -0,0 +1,56 @@
+/*
+        Title :Shared Pointer
+        Description/Usage:
+            Shared Pointer is a smart pointer that allows the objects to share the resources.
+            Automatically freed when the scope ends.     
+               
+*/
+
+
+
+#include<iostream>
+using namespace std;
+
+class A{
+
+    public:
+      int data;
+      A(int a=0){
+        this->data=a;
+      }
+      void print(){
+        cout << this->data << "\n";
+      }
+
+};
+
+
+
+
+
+
+int main(){
+
+    shared_ptr<A> a(new A(10));
+    shared_ptr<A> b(a);
+    shared_ptr<A> c;
+    c=b;
+
+    a->print(); //prints 10
+    b->print(); //prints 10
+    c->print();//prints 10
+
+    b->data=50;
+
+    a->print(); // prints 50
+    b->print();//prints 50
+    c->print();//prints 50
+
+    c->data=150;
+     a->print(); // prints 150
+    b->print();//prints 150
+    c->print();//prints 150
+
+
+    return 0;
+}
--- UniquePointer.cpp
+++ UniquePointer.cpp
@@ -4,3 +4,41 @@
     It means the multiple objects can't share the same resource . It can be only moved or in other words, the ownership
     can be transferred to  other object using std::move().
 */
+
+#include<iostream>
+
+using namespace std;
+
+class A{
+    public:
+    int a=0;;
+    A(int a=0){
+        this->a=a;
+    }
+    void print(){
+        cout<<a<<"\n";
+    }
+};
+int main(){
+
+        unique_ptr<A> aClass (new A(10)); //works , 1 owner
+
+        unique_ptr<A> bClass; // works
+        //bClass->print();//not initialized , not works .
+        //bClass=aClass; // can't copy , compilation error . Because uniqueptr resources cant be shared
+
+        unique_ptr<A> cClass (new A(160));//works , independent object with its own resource.
+        cClass->print();//works
+
+        
+
+        bClass=std::move(aClass);//works because ownership transferred;
+
+     //  aClass->print(); // Error. no more accessible
+        bClass->print();
+
+        cout<<aClass;
+        cout<<bClass;
+
+    return 0;
+}
